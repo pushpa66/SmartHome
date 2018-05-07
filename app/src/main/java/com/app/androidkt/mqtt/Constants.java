08ecd7c71a5c611cdc0122d983a806e0599b88f5
Pushpe <pushpakumara.14@cse.mrt.ac.lk>
2018-05-07 20:27:46 +0530

    initial commit

---
+++ app/src/main/java/com/app/androidkt/mqtt/Constants.java
@@ -0,0 +1,11 @@
+package com.app.androidkt.mqtt;
+
+public class Constants {
+
+    public static final String MQTT_BROKER_URL = "tcp://iot.eclipse.org:1883";
+
+    public static final String PUBLISH_TOPIC = "androidkt/topic";
+
+    public static final String CLIENT_ID = "smartHome";
+}
+
